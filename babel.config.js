82e298bc0bdca4c9eb21ce66721d00d16d652d38
Matheus <mmortari@lemontech.com.br>
2019-08-14 15:54:55 -0300

    Criando componente raiz

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
   presets: [
     "@babel/preset-env", // Responsável por alterar as propriedades do js que o navegador não entende
     "@babel/preset-react", // Responsável por alterar as propriedades do react que o navegador não entende
