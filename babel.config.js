89804bfd8fd7b406a9216dd4c41f3ccba5b50349
Matheus <mmortari@lemontech.com.br>
2019-08-14 13:36:53 -0300

    Configurando Estrutura

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.export = {
+  presets: [
+    "@babel/preset-env", // Responsável por alterar as propriedades do js que o navegador não entende
+    "@babel/preset-react", // Responsável por alterar as propriedades do react que o navegador não entende
+  ]
+}
